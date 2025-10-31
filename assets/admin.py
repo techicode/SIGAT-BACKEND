8c15da6ed5840f0991a0e50674cdc1c5cac730df
Luis <techicode@outlook.com>
2025-10-31 20:07:42 -0300

    feat: se personaliza panel de admin (assets) se permitea agregar gpu y disco duro directamente

--- assets/admin.py
+++ assets/admin.py
@@ -2,7 +2,35 @@
 from django.contrib import admin
 from .models import Asset, ComputerDetail, StorageDevice, GraphicsCard
 
-admin.site.register(Asset)
+
+class StorageDeviceInline(admin.TabularInline):
+    model = StorageDevice
+    extra = 1
+
+
+class GraphicsCardInline(admin.TabularInline):
+    model = GraphicsCard
+    extra = 1
+
+
+class AssetAdmin(admin.ModelAdmin):
+
+    list_display = ("inventory_code", "asset_type", "status", "employee", "department")
+
+    list_filter = ("status", "asset_type", "department")
+
+    search_fields = (
+        "inventory_code",
+        "serial_number",
+        "employee__first_name",
+        "employee__last_name",
+        "employee__rut",
+    )
+
+    inlines = [StorageDeviceInline, GraphicsCardInline]
+
+
+admin.site.register(Asset, AssetAdmin)
 admin.site.register(ComputerDetail)
 admin.site.register(StorageDevice)
 admin.site.register(GraphicsCard)
