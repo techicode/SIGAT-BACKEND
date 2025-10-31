b9ad96fb1e5df6ef0a4d66e76c394edb5f3b62ee
Luis <techicode@outlook.com>
2025-10-31 12:31:59 -0300

    feat: se modifica la secret_key, pasa a ser una variable de entorno .env

--- sigat/settings.py
+++ sigat/settings.py
@@ -10,17 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import environ
+import os
 from pathlib import Path
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-))y!8opcsefo136d57^&e4jd#do10uw^mpf$gn*l4!eng980@j"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
