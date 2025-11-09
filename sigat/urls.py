2d6e43ad44a9bfb6d49b89975b529aaa36c90c8b
Luis <techicode@outlook.com>
2025-11-09 20:14:08 -0300

    feat: se añaden tokens para interactuar con frontend

--- sigat/urls.py
+++ sigat/urls.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView as OriginalTokenObtainPairView,
+)
+from users.serializers import MyTokenObtainPairSerializer
+
+
+class MyTokenObtainPairView(OriginalTokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("users.urls")),
     path("api/", include("assets.urls")),
--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Department, Employee, CustomUser
 
 
@@ -73,3 +74,14 @@ class ChangePasswordSerializer(serializers.Serializer):
 
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token["username"] = user.username
+        token["role"] = user.role
+
+        return token
