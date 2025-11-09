c3532b6892878dd10629ed23fc1376b7cb7e68a0
Luis <techicode@outlook.com>
2025-11-09 17:25:25 -0300

    feat: se agregan restriccion de roles para los assets

--- assets/views.py
+++ assets/views.py
@@ -1,19 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Asset
 from .serializers import AssetListSerializer, AssetDetailSerializer
 
 
 class AssetViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows departments to be viewed or edited.
+    API endpoint that allows assets to be viewed or edited.
     """
 
     queryset = Asset.objects.all().order_by("inventory_code")
+    permission_classes = [IsAuthenticated]
 
     lookup_field = "inventory_code"
 
     def get_serializer_class(self):
-
         if self.action == "list":
             return AssetListSerializer
 
